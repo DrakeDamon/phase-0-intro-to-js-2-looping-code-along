7927def35f02184085fdbcf93b78488c34bc1415
Drake Damon <dddamon06@gmail.com>
2024-03-24 20:22:25 -0400

    Completed loop assignment by creating various loops

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(names, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+countDown(10);
